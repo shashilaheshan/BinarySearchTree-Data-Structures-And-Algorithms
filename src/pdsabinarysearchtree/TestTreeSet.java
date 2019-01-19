603270113632560286c2ef3351d055862917ab54
shashilaheshan <ebayshashila@gmail.com>
2019-01-19 14:03:01 +0530

    TreeSet Example Added

--- src/pdsabinarysearchtree/TestTreeSet.java
+++ src/pdsabinarysearchtree/TestTreeSet.java
@@ -23,15 +23,77 @@ public class TestTreeSet {
         //Maintain ascending order
         //stores in a red black tree
         TreeSet<String> al=new TreeSet<>();
+        TreeSet<Integer> al2=new TreeSet<>();
+        TreeSet<Student> al3=new TreeSet<>();
+        //adding String
         al.add("Ravi");
         al.add("Lahiru");
         al.add("Ravi");
         al.add("heshan");
         
-        Iterator<String> itr=al.iterator();
-        while(itr.hasNext()){
-            System.out.println(itr.next());
+        
+      int a[]={2,3,23,43};
+      for(int i=0;i<a.length;i++){
+         al2.add(a[i]);
+      
+      }
+      //adding objects
+      al3.add(new Student("shasila"));
+      al3.add(new Student("heshshshshshshsh"));
+      
+      
+        
+        // al.remove("heshan");
+        
+        //Adding String
+        
+//        Iterator<String> itr=al.iterator();
+//        while(itr.hasNext()){
+//            System.out.println(itr.next());
+//        }
+//adding Integer
+//         Iterator<Integer> itr2=al2.iterator();
+//        while(itr2.hasNext()){
+//            System.out.println(itr2.next());
+//        }
+        for(Student s:al3){
+            System.out.println(s.getName());
         }
+        
+        
+       
     }
     
+}
+class Student implements Comparable<Student>{
+
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    
+    @Override
+    public int compareTo(Student o) {
+        
+       if(this.getName().length()> o.getName().length()) {
+            return -1;
+        } else if (this.getName().length()< o.getName().length()) {
+            return 1;
+        } else {
+            return 0;
+        }
+//To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+
 }
